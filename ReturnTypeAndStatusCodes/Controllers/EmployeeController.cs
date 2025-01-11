2cf2a100319ddc10c6635a09e70ce2383786e1b3
vijay <vijay.jfsom@gmail.com>
2025-01-11 22:20:47 +0530

    Controller Action Return Types in ASP.NET Core Web API Initial Commit

---
+++ ReturnTypeAndStatusCodes/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReturnTypeAndStatusCodes.Models;
+
+namespace ReturnTypeAndStatusCodes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("Name")]
+        public string GetName()
+        {
+            return "Return from GetName";
+        }
+
+        [HttpGet("Details")]
+        public Employee GetEmployeeDetails()
+        {
+            return new Employee()
+            {
+                Id = 1001,
+                Name = "Anurag",
+                Age = 28,
+                City = "Mumbai",
+                Gender = "Male",
+                Department = "IT"
+            };
+        }
+
+        [HttpGet("All")]
+        public List<Employee> GetAllEmployee()
+        {
+            return new List<Employee>()
+            {
+                new Employee(){ Id = 1001, Name = "Anurag", Age = 28, City = "Mumbai", Gender = "Male", Department = "IT" },
+                new Employee(){ Id = 1002, Name = "Pranaya", Age = 28, City = "Delhi", Gender = "Male", Department = "IT" },
+                new Employee(){ Id = 1003, Name = "Priyanka", Age = 27, City = "BBSR", Gender = "Female", Department = "HR"},
+            };
+        }
+    }
+}
