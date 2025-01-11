fca90bc2386983bc064c6e5724c98fb162d1eef3
vijay <vijay.jfsom@gmail.com>
2025-01-11 22:49:02 +0530

    Response in case of IActionResult have not Explicitly casted the response

--- ReturnTypeAndStatusCodes/Controllers/EmployeeController.cs
+++ ReturnTypeAndStatusCodes/Controllers/EmployeeController.cs
@@ -1,42 +1,88 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ReturnTypeAndStatusCodes.Models;
-
 namespace ReturnTypeAndStatusCodes.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        [HttpGet("Name")]
-        public string GetName()
-        {
-            return "Return from GetName";
-        }
-
-        [HttpGet("Details")]
-        public Employee GetEmployeeDetails()
-        {
-            return new Employee()
-            {
-                Id = 1001,
-                Name = "Anurag",
-                Age = 28,
-                City = "Mumbai",
-                Gender = "Male",
-                Department = "IT"
-            };
-        }
-
         [HttpGet("All")]
-        public IEnumerable<Employee> GetAllEmployee()
+        //As we are going to return Ok, StatusCode, and NotFound Result from this action method
+        //So, we are using IActionResult as the return type of this method
+        public IActionResult GetAllEmployee()
         {
-            return new List<Employee>()
+            try
             {
-                new Employee(){ Id = 1001, Name = "Anurag", Age = 28, City = "Mumbai", Gender = "Male", Department = "IT" },
-                new Employee(){ Id = 1002, Name = "Pranaya", Age = 28, City = "Delhi", Gender = "Male", Department = "IT" },
-                new Employee(){ Id = 1003, Name = "Priyanka", Age = 27, City = "BBSR", Gender = "Female", Department = "HR"},
-            };
+                //In Real-Time, you will get the data from the database
+                //Here, we have hardcoded the data
+                var listEmployees = new List<Employee>()
+                {
+                    new Employee(){ Id = 1001, Name = "Anurag", Age = 28, City = "Mumbai", Gender = "Male", Department = "IT" },
+                    new Employee(){ Id = 1002, Name = "Pranaya", Age = 28, City = "Delhi", Gender = "Male", Department = "IT" },
+                    new Employee(){ Id = 1003, Name = "Priyanka", Age = 27, City = "BBSR", Gender = "Female", Department = "HR"},
+                };
+
+                //If at least one Employee is Present return OK status code and the list of employees
+                if (listEmployees.Any())
+                {
+                    return Ok(listEmployees);
+                }
+                else
+                {
+                    //If no Employee is Present return Not Found Status Code
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (optional)
+                // _logger.LogError(ex, "An error occurred while getting all employees.");
+
+                // Return a 500 Internal Server Error status code
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
+
+        //As the following method is going to return Ok, Internal Server Error and NotFound Result
+        //So, we are using IActionResult as the return type of this method
+        [HttpGet("{Id}")]
+        public IActionResult GetEmployeeDetails(int Id)
+        {
+            try
+            {
+                //In Real-Time, you will get the data from the database
+                //Here, we have hardcoded the data
+                var listEmployees = new List<Employee>()
+                {
+                    new Employee(){ Id = 1001, Name = "Anurag", Age = 28, City = "Mumbai", Gender = "Male", Department = "IT" },
+                    new Employee(){ Id = 1002, Name = "Pranaya", Age = 28, City = "Delhi", Gender = "Male", Department = "IT" },
+                    new Employee(){ Id = 1003, Name = "Priyanka", Age = 27, City = "BBSR", Gender = "Female", Department = "HR"},
+                };
+
+                //Fetch the Employee Data based on the Employee Id
+                var employee = listEmployees.FirstOrDefault(emp => emp.Id == Id);
+
+                //If Employee Exists Return OK with the Employee Data
+                if (employee != null)
+                {
+                    return Ok(employee);
+                }
+                else
+                {
+                    //If Employee Does Not Exists Return NotFound
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (optional)
+                // _logger.LogError(ex, "An error occurred while getting all employees.");
+
+                // Return a 500 Internal Server Error status code
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+
     }
 }
